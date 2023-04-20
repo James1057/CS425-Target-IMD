0f5eb6f09db3e1725500961c4c28f7992f0e3d24
James Benson <51465877+James1057@users.noreply.github.com>
2023-04-19 21:24:15 -0500

    Add files via upload

---
+++ CS425_Target_IMD/Home.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace CS425_Target_IMD
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Home));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.UserNameHome = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.PassWordHome = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(504, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UserNameHome
+            // 
+            this.UserNameHome.Location = new System.Drawing.Point(65, 140);
+            this.UserNameHome.Name = "UserNameHome";
+            this.UserNameHome.Size = new System.Drawing.Size(100, 20);
+            this.UserNameHome.TabIndex = 2;
+            this.UserNameHome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // PassWordHome
+            // 
+            this.PassWordHome.Location = new System.Drawing.Point(65, 222);
+            this.PassWordHome.Name = "PassWordHome";
+            this.PassWordHome.Size = new System.Drawing.Size(100, 20);
+            this.PassWordHome.TabIndex = 3;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 561);
+            this.Controls.Add(this.PassWordHome);
+            this.Controls.Add(this.UserNameHome);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Home_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox UserNameHome;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox PassWordHome;
+    }
+}
+
---
+++ CS425_Target_IMD/Home.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS425_Target_IMD
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //String UserName = textBox1.Text;
+            String UserName = UserNameHome.Text;
+            String PassWord = PassWordHome.Text;
+            MessageBox.Show("User Name: " + UserName + "\nPassword: " +PassWord);
+            //EmployeePage F = new EmployeePage();
+            //Hide();
+            //F.ShowDialog();
+            //Close();
+        }
+
+        private void Home_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //String UserName = textBox1.Text;
+            //MessageBox.Show("User Name: " + UserName);
+        }
+    }
+}
