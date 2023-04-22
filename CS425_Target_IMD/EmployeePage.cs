e39db15cb5b45b15f39d33438da9a1337e65bb52
mochalattez <cmocha22@gmail.com>
2023-04-21 23:24:42 -0500

    new employee page

--- CS425_Target_IMD/EmployeePage.cs
+++ CS425_Target_IMD/EmployeePage.cs
@@ -34,15 +34,26 @@ namespace CS425_Target_IMD
         {
             //Loading elements on the home page
             //MessageBox.Show(UserNameGlobal);
-            textBox1.Text = "Thank you for Signing In";
+            textBox1.Text = "Thank You for Signing In";
             textBox1.ReadOnly = true;
             textBox2.Text = "Please make a selection:";
             textBox2.ReadOnly = true;
-            Hours F = new Hours();
-            F.UserNameGlobal = UserNameGlobal;
-            //Hide();
-            F.ShowDialog();
-            //Close();
+            textBox3.Text = "Your Store Info";
+            textBox3.ReadOnly = true;
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = DBUtils.GetDBConnection();
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            var stm = "select store_hours, location, store_manager, department_name from store inner join employee where employee.SID = Store.SID and EID = '" + UserNameGlobal + "'";
+            var cmd = new MySqlCommand(stm, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.DataSource = table;
+            conn.Close();
 
         }
 
@@ -59,6 +70,46 @@ namespace CS425_Target_IMD
         private void BackToEmployee_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hours F = new Hours();
+            F.UserNameGlobal = UserNameGlobal;
+            Hide();
+            F.ShowDialog();
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = DBUtils.GetDBConnection();
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+
+            conn.Open();
+
+            var stm3 = "select role from employee where EID = '" + UserNameGlobal + "'";
+            var cmd3 = new MySqlCommand(stm3, conn);
+            MySqlDataReader rdr = cmd3.ExecuteReader();
+            rdr.Read();
+
+            var erole = rdr.GetString(0);
+
+            if (erole == "Manager") {
+                MessageBox.Show("Access Granted");
+            } else {
+                MessageBox.Show("Access Denied");
+            };
+            
+            conn.Close();
+
         }
     }
 }
