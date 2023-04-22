dfbaa8d6337b5fc3c6d6fd4f96b8b6f4e7ed72e7
mochalattez <cmocha22@gmail.com>
2023-04-22 00:59:14 -0500

    updated departments

--- CS425_Target_IMD/Department.Designer.cs
+++ CS425_Target_IMD/Department.Designer.cs
@@ -28,12 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Department));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.quitbutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(478, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(318, 148);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.5F);
+            this.textBox1.Location = new System.Drawing.Point(62, 93);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(368, 38);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.Location = new System.Drawing.Point(510, 219);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 65);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Store Bank Accounts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.Location = new System.Drawing.Point(510, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 65);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Department Products";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.5F);
+            this.button3.Location = new System.Drawing.Point(510, 436);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // quitbutton
+            // 
+            this.quitbutton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.5F);
+            this.quitbutton.Location = new System.Drawing.Point(658, 436);
+            this.quitbutton.Name = "quitbutton";
+            this.quitbutton.Size = new System.Drawing.Size(100, 100);
+            this.quitbutton.TabIndex = 5;
+            this.quitbutton.Text = "Quit";
+            this.quitbutton.UseVisualStyleBackColor = true;
+            this.quitbutton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(62, 219);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(368, 233);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Department
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(834, 561);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.quitbutton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Department";
+            this.Text = "Store Details";
+            this.Load += new System.EventHandler(this.Department_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button quitbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- CS425_Target_IMD/Department.cs
+++ CS425_Target_IMD/Department.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,73 @@ namespace CS425_Target_IMD
         public Department()
         {
             InitializeComponent();
+        }
+
+        private void Department_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "   Store Details for Managers";
+            textBox1.ReadOnly = true;
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString = DBUtils.GetDBConnection();
+            conn = new MySql.Data.MySqlClient.MySqlConnection();
+            conn.ConnectionString = myConnectionString;
+            conn.Open();
+            var stm5 = "SELECT fname,lname FROM employee WHERE EID = '" + UserNameGlobal + "'";
+            var cmd5 = new MySqlCommand(stm5, conn);
+            MySqlDataReader rdr = cmd5.ExecuteReader();
+            rdr.Read();
+            var name = rdr.GetString(0) + ' ' + rdr.GetString(1);
+            conn.Close();
+            conn.Open();
+
+            var stm4 = "select department.department_name, category, in_store_locaiton from department inner join store where department.department_name = store.department_name and store_manager = '" + name + "'";
+            var cmd4 = new MySqlCommand(stm4, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd4);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.DataSource = table;
+            conn.Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StoreBanking F = new StoreBanking();
+            F.UserNameGlobal = UserNameGlobal;
+            Hide();
+            F.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EmployeePage F = new EmployeePage();
+            F.UserNameGlobal = UserNameGlobal;
+            Hide();
+            F.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProductDepartment F = new ProductDepartment();
+            F.UserNameGlobal = UserNameGlobal;
+            Hide();
+            F.ShowDialog();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
